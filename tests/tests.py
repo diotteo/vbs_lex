aef2e880cb713074ddc20ee649aa5e03a7950f8b
Olivier Diotte <olivier@diotte.ca>
2022-01-16 17:50:01 -0500

    Fix test import statement

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestPass(unittest.TestCase):
     def test_lex_for_step(self):
-        from src.vbs_lex.lexing import lex_str, LexemeType, Lexeme
+        from vbs_lex.lexing import lex_str, LexemeType, Lexeme
         with open('example_code/for_step.vbs') as f:
             s = f.read()
             lxms = lex_str(s, f.name)
